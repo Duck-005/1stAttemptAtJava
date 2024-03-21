7777effb905bb96ff7762a59651f8ca29c549fa0
Duck-005 <pondesai87@gmail.com>
2024-03-21 18:26:14 +0530

    added a custom encryptor based on shifting of unicode values just like Caesar cipher.

---
+++ naive cipher/customEncryptor.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.lang.*;
+import java.io.*;
+public class customEncryptor {
+    final private int key = 3;
+    String shiftEncrypt(String clearText){
+        int[] ASCII_val = new int[clearText.length()];
+        char[] temp = new char[clearText.length()];
+        for(int i = 0; i < clearText.length(); i++){
+            ASCII_val[i] = clearText.charAt(i);
+            if(ASCII_val[i] != 32) {
+                int shifted = ASCII_val[i] << key;
+                temp[i] = (char) shifted;
+            }
+            else temp[i] = (char) 1023;
+        }
+        return Arrays.toString(temp);
+    }
+    void manageFile(String cypherText, String clearText){
+        try {
+            File crypto = new File("vault.txt");
+            crypto.createNewFile();
+            FileWriter fileWriter = new FileWriter("vault.txt", true);
+            fileWriter.write("Clear Text : " + clearText + "\n");
+            fileWriter.write("Cypher Text : " + cypherText + "\n\n");
+            fileWriter.close();
+        }
+        catch(IOException e){
+            System.out.print("An error occurred");
+        }
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        customEncryptor crypto = new customEncryptor();
+        System.out.print("Enter the clear text : ");
+        String clearText = sc.nextLine();
+        String cypherText = crypto.shiftEncrypt(clearText);
+        crypto.manageFile(cypherText, clearText);
+    }
+}
